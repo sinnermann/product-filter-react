8f3989ea857f7663dd64eb825148e7ad4c62e055
sinnermann <ealijev@gmail.com>
2024-12-19 19:49:15 +0400

    project commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import ProductList from './components/product/ProductList'
+
+function App() {
+  
+
+  return (
+    <div>
+     <ProductList />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/search/Search.jsx
@@ -0,0 +1,11 @@
+import "./Search.css";
+
+const Search = ({inputValue, onInputChange}) => {
+  return (
+    <div className="--form-control">
+      <input type="text" placeholder="Search Products" value={inputValue} onChange={onInputChange} />
+    </div>
+  );
+};
+
+export default Search;
